93bda7e4b6850ee04b45bca6cb425044e2e4198a
Elizabeth Fileti <bettyfileti@macbookpro.mynetworksettings.com>
2025-12-29 16:53:48 -0500

    corrections

--- index.js
+++ index.js
@@ -15,23 +15,37 @@ server.listen(port, () => {
 let io = require("socket.io");
 io = new io.Server(server);
 
+let clientCount = 0;
+
 //socket connection
 io.sockets.on("connection", (socket) => {
     console.log("We have a new client: " + socket.id);
-
-    socket.on("disconnect", ()=> {
-        console.log("Client disconnected: " + socket.id);
-    });
-
-    //listen for cursor position data
-    socket.on("data", (data) => {
-        //send to all clients, including myself
-        io.sockets.emit("draw-data", data);
+    
+    // Send socket ID and color assignment to new client
+    socket.emit("getMySocketId", {
+        clientID: socket.id,
+        clientCount: clientCount
     });
     
-    //listen for points data
-    socket.on("points", (data) => {
-        //send to all clients
-        io.sockets.emit("points-data", data);
+    clientCount++;
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected: " + socket.id);
+        clientCount--;
+        
+        // Tell all other clients this player left
+        socket.broadcast.emit("clientLeft", socket.id);
+    });
+
+    // Listen for mouse position data
+    socket.on("weHaveNewMouseData", (data) => {
+        // Send to all OTHER clients (not back to sender)
+        socket.broadcast.emit("draw-data", data);
+    });
+    
+    // Listen for points updates
+    socket.on("weHaveUpdatedPoints", (data) => {
+        // Send to all clients including sender
+        io.sockets.emit("draw-points", data);
     });
 });
